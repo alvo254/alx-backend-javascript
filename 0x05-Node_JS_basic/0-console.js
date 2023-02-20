fcee0da94d9e074b107ad796b8e6f3f0c1a3ebfa
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-02-20 22:15:54 +0300

    console

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Bezaleel Olakunori <https://github.com/alvo254>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
