7091fc15b5bb75f6d16d906f35e1708fa6995dd1
alvo254 <41018786+alvo254@users.noreply.github.com>
2023-02-20 22:28:09 +0300

    full server

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,10 +5,6 @@ import readDatabase from '../utils';
  */
 const VALID_MAJORS = ['CS', 'SWE'];
 
-/**
- * Contains the student-related route handlers.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
- */
 class StudentsController {
   static getAllStudents(request, response) {
     const dataPath = process.argv.length > 2 ? process.argv[2] : '';
